1c7861dd16ecde4c19d92735a3345388b666fb49
DlyaTebe-art <245399988+DlyaTebe-art@users.noreply.github.com>
2025-11-21 17:00:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MODERATOR_CHAT_ID = int(os.getenv("MODERATOR_CHAT_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
