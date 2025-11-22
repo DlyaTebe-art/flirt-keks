6ba4e5dc958b4b682f08ea0fa66b5c13e61de7ea
DlyaTebe-art <245399988+DlyaTebe-art@users.noreply.github.com>
2025-11-22 13:29:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,33 +3,40 @@ import os
 from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
+
 from database import init_db
-
-async def main():
-    await init_db()
-
-# завантажуємо .env
-load_dotenv()
-
 from handlers.create_ad import create_ad_router
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+
 async def main():
-    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+    # ініціалізація БД
+    await init_db()
+
+    # створення бота з новим підходом
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode="HTML")
+    )
+
     dp = Dispatcher(storage=MemoryStorage())
 
     # реєструємо роутер
     dp.include_router(create_ad_router)
 
     print("Бот запущено...")
+
     try:
         await dp.start_polling(bot)
     finally:
         await dp.storage.close()
         await bot.session.close()
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
